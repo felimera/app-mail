fd76a2909340bb3a0c42a4dcd7253809bbb414dc
andres.mera <feli041089@gmail.com>
2024-01-31 08:32:11 -0500

    dev : Definicion del modelo Mail.

---
+++ src/main/java/com/project/appmail/model/MailStructure.java
@@ -0,0 +1,11 @@
+package com.project.appmail.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MailStructure {
+    private String subject;
+    private String message;
+}
