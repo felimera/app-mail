b7f3e9e024859312b598cd862fe4a7a5f7e95828
andres.mera <feli041089@gmail.com>
2024-01-31 08:21:31 -0500

    master : Creacion del proyecto mail.

---
+++ src/test/java/com/project/appmail/AppMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.appmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
