5df550f1cdfad9eac566c62fe07bd62770d9130f
andres.mera <feli041089@gmail.com>
2024-01-31 08:49:56 -0500

    dev : Creacion del controller con el endpoint de mail.

---
+++ src/main/java/com/project/appmail/controller/MailController.java
@@ -0,0 +1,25 @@
+package com.project.appmail.controller;
+
+import com.project.appmail.model.MailStructure;
+import com.project.appmail.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api/mail")
+public class MailController {
+
+    private MailService mailService;
+@Autowired
+    public MailController(MailService mailService) {
+        this.mailService = mailService;
+    }
+
+    @PostMapping(path = "/send/{mail}")
+    public ResponseEntity<String> sendMail(@PathVariable(name = "mail") String mail, @RequestBody MailStructure mailStructure)
+    {
+mailService.sendMail(mail,mailStructure);
+return  ResponseEntity.ok("Successfully sent the mail!!");
+    }
+}
