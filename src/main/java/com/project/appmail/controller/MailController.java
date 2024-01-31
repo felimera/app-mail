6bf9a37955470fc9321e4e3e4b26e807d302d6c6
andres.mera <feli041089@gmail.com>
2024-01-31 09:09:17 -0500

    dev : Agregando el Mapper.

--- src/main/java/com/project/appmail/controller/MailController.java
+++ src/main/java/com/project/appmail/controller/MailController.java
@@ -1,7 +1,8 @@
 package com.project.appmail.controller;
 
-import com.project.appmail.model.MailStructure;
+import com.project.appmail.model.dto.MailStructureDTO;
 import com.project.appmail.service.MailService;
+import com.project.appmail.service.mapper.MailStructureMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +19,8 @@ public class MailController {
     }
 
     @PostMapping(path = "/send/{mail}")
-    public ResponseEntity<String> sendMail(@PathVariable(name = "mail") String mail, @RequestBody MailStructure mailStructure) {
-        mailService.sendMail(mail, mailStructure);
+    public ResponseEntity<String> sendMail(@PathVariable(name = "mail") String mail, @RequestBody MailStructureDTO mailStructureDTO) {
+        mailService.sendMail(mail, MailStructureMapper.INSTANCE.fromDomainModel(mailStructureDTO));
         return ResponseEntity.ok("Successfully sent the mail!!");
     }
 }
